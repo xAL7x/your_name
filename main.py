08b181c0cc1f29f7f96c09d9e3e163a759b6984b
xAL7x[F-15] <76629405+xAL7x@users.noreply.github.com>
2023-03-17 11:33:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print('''
+ _   _
+| \ | | __ _ _ __ ___   ___
+|  \| |/ _` | '_ ` _ \ / _ \
+| |\  | (_| | | | | | |  __/
+|_| \_|\__,_|_| |_| |_|\___|
+
+''')
+
+yourname=input('Enter your name: ')
+print(f'Your name is: {yourname}')
